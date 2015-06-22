0a2c50f1e09ed739ee087fa86a7bb16f223dc3c5
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-06-22 22:51:58 +0300

    generate own Test World `rails g swat:test_world:install AppTestWorld`

---
+++ lib/swat/AppTestWorld.rb
@@ -0,0 +1,26 @@
+require 'sw2at-tw'
+require 'timecop'
+
+# Instance of this class will be available as @tw variable inside of RSpec context
+
+class AppTestWorld < Swat::TestWorld::Base
+# You can include your own modules here
+
+  # Define your options and situation below
+  BASE_OPTIONS = {}
+  DEFAULT_OPTIONS = {}
+  SITUATIONS = {}
+
+  def init_situation
+    # implement your situations
+  end
+
+  def before_each(example, context)
+    # do something
+  end
+
+  def after_each(example, context)
+    # do something
+  end
+
+end
